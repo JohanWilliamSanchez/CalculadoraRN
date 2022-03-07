c4348add78ae1e07d9edefabd10965e576d7ff1a
Laura Garcia <lauravictoriagarcia@gmail.com>
2022-03-07 13:32:21 -0500

    No sirven los botones jajaja

--- App.js
+++ App.js
@@ -1,20 +1,80 @@
+import React from "react";
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import reactDom from 'react-dom';
+import { StyleSheet, Text, View, SafeAreaView, TextInput, Button, Alert } from 'react-native';
 
 export default function App() {
+
+  const [number, onChangeNumber] = React.useState(null);
+
   return (
-    <View style={styles.container}>
-      <Text>Nuestra primera calculadora en RN Hola Mundo</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.titulo}>Calculadora</Text>
       <StatusBar style="auto" />
-    </View>
+
+      <TextInput
+        style={styles.input}
+        onChangeText={(numero1) => this.setState({ numero1 })}
+        value={number}
+        placeholder="Numero 1"
+        keyboardType="numeric"
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeNumber}
+        value={number}
+        placeholder="Numero 2"
+        keyboardType="numeric"
+      />
+      <Button
+        title="Suma"
+        color="#841584"
+        onPress={() => Alert.alert('Se oprimio el boton')}
+      />
+      <View style={styles.fixToText}>
+        <Button
+          title="Resta"
+          onPress={() => Alert.alert('Left button pressed')}
+        />
+        <Button
+          title="Multiplicacion"
+          onPress={() => Alert.alert('Right button pressed')}
+        />
+        <Button
+          title="Division"
+          onPress={() => Alert.alert('Left button pressed')}
+        />
+        <Button
+          title="Potenciacion"
+          onPress={() =>console.log('Right button pressed')}
+        />
+      </View>
+      
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 0.5,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  titulo: {
+    fontSize: 50,
+    color: 'red',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  fixToText: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
